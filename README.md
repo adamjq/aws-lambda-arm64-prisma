e64b8d1739907808bf8ed92fd48ee296a834f580
Adam Quigley <adamjohnquigley@gmail.com>
2022-11-01 12:37:47 +1100

    feat: add prisma to lambda

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Minimal reproduction to run Prisma on AWS Lambda with arm64 architecture
 npm install
 npx serverless deploy
 ```
+
+**Note** this packages a SQLite `dev.db` file in the Lambda function which is not writable to as it is not under the Lambda `/tmp` directory. The purpose of the project is to showcase how to package Prisma binaries for AWS Lambda architectures.
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import { Context, APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
 
 export const lambdaHandler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+
+    // creating user
+    const user = await createPrismaUser();
+
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'hello world',
+            user,
         }),
     };
 };
+
+const createPrismaUser = async () => {
+    return prisma.user.create({
+        data: {
+            name: 'Alice',
+            email: 'alice@prisma.io',
+        },
+    })
+}
